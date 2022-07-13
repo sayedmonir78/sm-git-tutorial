2de4ace0e12b1246e1756c87b724ada7eb0d761d
SayedMonir <mmtutorial427@gmail.com>
2022-07-13 22:17:23 +0600

    Added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class Hello{
   public static void main(string[]args){
-  system.out.printIn("VS code is Awesome...")
+  system.out.printIn("VS code is Awesome...");
+
+  greeting("Sayed Monir");
+  }
+
+  public static void greeting(string name){
+    system.out.printIn("Welcom "+name);
   }
 }
